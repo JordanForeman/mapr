8fa2b19d591e543537d9a536f369810c10f642e5
Jordan Foreman <me@jordanforeman.com>
2019-11-13 17:53:36 -0600

    fix: r53 dns zone

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -3,13 +3,13 @@ data "aws_route53_zone" "hosted_zone" {
 }
 
 resource "aws_route53_record" "dns_record" {
-    zone_id = "${data.aws_route53_zone.hosted_zone.zone_id}"
+    zone_id = "${aws_cloudfront_distribution.cloudfront_distribution.hosted_zone_id}"
     name = "${var.app_name}.${var.root_domain}"
     type = "A"
 
     alias {
         name = "${aws_cloudfront_distribution.cloudfront_distribution.domain_name}"
-        zone_id = "${data.aws_route53_zone.hosted_zone.zone_id}"
+        zone_id = "${aws_cloudfront_distribution.cloudfront_distribution.hosted_zone_id}"
         evaluate_target_health = false
     }
 }

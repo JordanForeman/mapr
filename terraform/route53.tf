b7797148c78c4bdc43880843cdf4cd0c7b23fdc5
Jordan Foreman <me@jordanforeman.com>
2019-11-13 19:49:34 -0600

    fix: route53 hosted zone id

--- terraform/route53.tf
+++ terraform/route53.tf
@@ -1,9 +1,10 @@
 data "aws_route53_zone" "hosted_zone" {
     name = "${var.root_domain}"
+    private_zone = false
 }
 
 resource "aws_route53_record" "dns_record" {
-    zone_id = "${aws_cloudfront_distribution.cloudfront_distribution.hosted_zone_id}"
+    zone_id = "${data.aws_route53_zone.hosted_zone.zone_id}"
     name = "${var.app_name}.${var.root_domain}"
     type = "A"
 

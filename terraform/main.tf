7ec61266ead84c2f146da0260c061bb25fba0550
Jordan Foreman <me@jordanforeman.com>
2019-11-13 18:05:06 -0600

    feat: remote tf state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "jf-terraform-state"
+    key    = "mapr/mapr.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
     version = "~> 2.0"
     region = "us-east-1"
